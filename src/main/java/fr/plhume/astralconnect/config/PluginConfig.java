875f11225b49ddf8592deedb6f0ec1a00e441904
Plhume <plhum.studiok@gmail.com>
2023-03-09 21:14:15 +0100

    Adding config in PluginConfig class

--- src/main/java/fr/plhume/astralconnect/config/PluginConfig.java
+++ src/main/java/fr/plhume/astralconnect/config/PluginConfig.java
@@ -9,4 +9,32 @@ import java.io.File;
 import java.io.IOException;
 
 public class PluginConfig {
+
+    private static File file;
+    private static Configuration config;
+
+    public static void createConfig() throws IOException {
+        if (!AstralConnect.getInstance().getDataFolder().exists()) {
+            AstralConnect.getInstance().getDataFolder().mkdir();
+        }
+
+        file = new File(AstralConnect.getInstance().getDataFolder(), "config.yml");
+
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
+
+            config.set("prefix", "&7[&3Astral&7] &r");
+            ConfigurationProvider.getProvider(YamlConfiguration.class).save(config, file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Configuration getConfig() {
+        return config;
+    }
 }
