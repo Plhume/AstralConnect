18dbf3f268d4ae51f36ac2821bd4f097e14c8e2d
Plhume <plhum.studiok@gmail.com>
2023-03-09 20:55:37 +0100

    Creating ChatModule system

--- src/main/java/fr/plhume/astralconnect/modules/ChatModule.java
+++ src/main/java/fr/plhume/astralconnect/modules/ChatModule.java
@@ -1,4 +1,10 @@
 package fr.plhume.astralconnect.modules;
 
+import fr.plhume.astralconnect.AstralConnect;
+
 public class ChatModule {
+
+    public static boolean isChatEnabled() {
+        return AstralConnect.getInstance().getConfig().getBoolean("modules.chat");
+    }
 }
