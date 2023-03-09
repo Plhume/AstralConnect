c798a879c2acaf114e233edd53ecd014d33d93f6
Plhume <plhum.studiok@gmail.com>
2023-03-09 20:43:24 +0100

    Update AstralConnect.java

--- src/main/java/fr/plhume/astralconnect/AstralConnect.java
+++ src/main/java/fr/plhume/astralconnect/AstralConnect.java
@@ -4,10 +4,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AstralConnect extends JavaPlugin {
 
+    private static AstralConnect instance;
+
+    public static AstralConnect getInstance() {
+        return instance;
+    }
+
     @Override
     public void onEnable() {
+        instance = this;
         saveDefaultConfig();
-        
+
         getLogger().info("============================");
         getLogger().info("= AstralConnect | Enabling =");
         getLogger().info("=   Loading version: " + getDescription().getVersion() + "   =");
