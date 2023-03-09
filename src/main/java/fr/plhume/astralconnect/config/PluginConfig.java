989cc8ca6fb8168807bb64d58888a3986e658b78
Plhume <plhum.studiok@gmail.com>
2023-03-09 21:14:00 +0100

    Creating PluginConfig file

---
+++ src/main/java/fr/plhume/astralconnect/config/PluginConfig.java
@@ -0,0 +1,12 @@
+package fr.plhume.astralconnect.config;
+
+import fr.plhume.astralconnect.AstralConnect;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class PluginConfig {
+}
