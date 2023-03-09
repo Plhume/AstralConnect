40fb46739bb3e3a91d55dde3b44909ea83f3f037
Plhume <plhum.studiok@gmail.com>
2023-03-09 21:22:44 +0100

    Update ChatModule.java

--- src/main/java/fr/plhume/astralconnect/modules/ChatModule.java
+++ src/main/java/fr/plhume/astralconnect/modules/ChatModule.java
@@ -1,19 +1,10 @@
 package fr.plhume.astralconnect.modules;
 
-import fr.plhume.astralconnect.AstralConnect;
-import org.bukkit.entity.Player;
-
-import java.util.Objects;
+import fr.plhume.astralconnect.config.PluginConfig;
 
 public class ChatModule {
 
-    public static boolean isChatEnabled() {
-        return AstralConnect.getInstance().getConfig().getBoolean("modules.chat");
-    }
-
-    public static String getBroadcastPrefix(Player player, String msg) {
-        return Objects.requireNonNull(AstralConnect.getInstance().getConfig().getString("messages.broadcast-prefix"))
-                .replace("%player%", player.getDisplayName())
-                .replace("%msg%", msg);
+    public static void setChatModule(boolean status) {
+        PluginConfig.getConfig().set("modules.chat", status);
     }
 }
