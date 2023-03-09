7435b1296d96e7d84e77d918c1fa3a5f0f960a8d
Plhume <plhum.studiok@gmail.com>
2023-03-09 20:58:29 +0100

    Creating broadcast prefix system

--- src/main/java/fr/plhume/astralconnect/modules/ChatModule.java
+++ src/main/java/fr/plhume/astralconnect/modules/ChatModule.java
@@ -1,10 +1,19 @@
 package fr.plhume.astralconnect.modules;
 
 import fr.plhume.astralconnect.AstralConnect;
+import org.bukkit.entity.Player;
+
+import java.util.Objects;
 
 public class ChatModule {
 
     public static boolean isChatEnabled() {
         return AstralConnect.getInstance().getConfig().getBoolean("modules.chat");
+    }
+
+    public static String getBroadcastPrefix(Player player, String msg) {
+        return Objects.requireNonNull(AstralConnect.getInstance().getConfig().getString("messages.broadcast-prefix"))
+                .replace("%player%", player.getDisplayName())
+                .replace("%msg%", msg);
     }
 }
