2baee45eaf5f9b22ec6c05c55eb6641a3a685db7
Plhume <plhum.studiok@gmail.com>
2023-03-09 19:59:57 +0100

    Initial Commit

---
+++ src/main/java/fr/plhume/astralconnect/AstralConnect.java
@@ -0,0 +1,17 @@
+package fr.plhume.astralconnect;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AstralConnect extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
